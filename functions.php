70c844fe02583b7018c4d04b3fcdce8824904e02
Evgeny Polyakov <evg_pol@yahoo.com>
2023-06-29 20:41:55 +0300

    added home page / get_posts function

--- functions.php
+++ functions.php
@@ -8,7 +8,9 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('animate', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css');
     wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/style.css');
 
-    // <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
+    wp_enqueue_script('jquery');
 
     wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), 'null', true);
     wp_enqueue_script('slick', get_template_directory_uri() . '/assets/js/slick.min.js', array('jquery'), 'null', true);
